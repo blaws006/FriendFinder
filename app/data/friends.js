04429780d1f57c4b6d066c588ac794aecd9ac205
blaws006 <blaws006>
2017-11-07 18:10:39 -0500

    Pushing it all

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendArray = [
+	{
+		name: "Chris Hemsworth",
+		photo: "https://uproxx.files.wordpress.com/2017/04/thor-ragnarok-1_marvel.jpg?quality=100&w=650&h=373",
+		scores: [
+			"5", 
+			"2", 
+			"5", 
+			"1", 
+			"2", 
+			"2", 
+			"1", 
+			"5", 
+			"1", 
+			"5"],
+}
+]
+
+module.exports = friendArray;
---
+++ server.js
@@ -0,0 +1,28 @@
+//Requiremnets///////////////////////////////////////////////
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+//Global Varibles///////////////////////////////////////////////
+
+//Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+//Routers//////////////////////////////////////////////////
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//Functions///////////////////////////////////////////////
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
