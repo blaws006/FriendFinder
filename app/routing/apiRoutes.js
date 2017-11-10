bace5b859ba787fabbdc154cc5079c65fe93aa46
blaws006 <blaws006>
2017-11-10 18:39:31 -0500

    ????

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,16 +2,20 @@ var friends = require("../data/friends");
 
 
 module.exports = function (app) {
-  app.get("/api/friends", function(req, res){
+  app.get("/api/friends", function (req, res) {
     res.json(friends);
   });
   console.log(friends);
 
-  app.post("/api/friends", function(req, res){
+  app.post("/api/friends", function (req, res) {
     friends.push(req.body);
-    for(var i = 0; i < friends.length; i++){
+    
+    for (var i = 0; i < friends.length; i++) {
+      var newFriend = friends[friends.length - 1].scores.map(Number);
+      console.log(newFriend);
       friends[i].scores = friends[i].scores.map(Number);
       console.log(friends[i].scores);
-    } 
+    }
+    
   });
 };
